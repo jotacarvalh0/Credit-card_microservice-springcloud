346165077dddb5e5ef505cdd812e19825c8d8bb7
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-06-30 15:14:13 -0300

    [mscustomers] Registering Microservice on Eureka Server.

--- mscustomers/src/main/java/io/github/jotacarvalh0/mscustomers/MscustomersApplication.java
+++ mscustomers/src/main/java/io/github/jotacarvalh0/mscustomers/MscustomersApplication.java
@@ -2,8 +2,10 @@ package io.github.jotacarvalh0.mscustomers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MscustomersApplication {
 
 	public static void main(String[] args) {
