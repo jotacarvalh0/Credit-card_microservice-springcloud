810d98a48bce11eb6b92e73f78aac944d093a209
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-06-30 14:57:13 -0300

    Customer Microservice Scaffolding

---
+++ mscustomers/src/main/java/io/github/jotacarvalh0/mscustomers/MscustomersApplication.java
@@ -0,0 +1,13 @@
+package io.github.jotacarvalh0.mscustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscustomersApplication.class, args);
+	}
+
+}
---
+++ mscustomers/src/test/java/io/github/jotacarvalh0/mscustomers/MscustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jotacarvalh0.mscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
